6d5a73b09fd3750e81ffffed690746d40fef8f35
TTTurbo <1119144202@qq.com>
2020-07-14 16:32:39 +0800

    feat:

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -41,7 +41,8 @@ module.exports = {
             updatePopup: {
               message: "发现新内容可用",
               buttonText: "刷新"
-            }
+            },
+            '@vuepress/back-to-top': true
         }
 
     },
