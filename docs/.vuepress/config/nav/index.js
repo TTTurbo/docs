86ea2b840d103b4e213e648671a2199a1efd729f
TTTurbo <1119144202@qq.com>
2020-07-18 20:23:18 +0800

    feat：

--- docs/.vuepress/config/nav/index.js
+++ docs/.vuepress/config/nav/index.js
@@ -11,30 +11,21 @@ module.exports = [
       }, {
           text: 'Common sites🎈',
           items: [{
-              text: 'Java SE API Documentation',
-              link: 'https://docs.oracle.com/javase/8/docs/api/index.html',
+              text: 'JavaScript',
+              link: 'https://developer.mozilla.org/zh-CN/docs/Web/JavaScript',
           }, {
-              text: 'Program Creek',
-              link: 'https://www.programcreek.com/',
+              text: 'ES6',
+              link: 'https://es6.ruanyifeng.com/',
           }, {
-              text: 'Spring',
-              link: 'https://spring.io/',
-          }, {
-              text: 'Stackoverflow',
-              link: 'https://stackoverflow.com/',
-          }, {
+              text: 'HTML',
+              link: 'https://developer.mozilla.org/zh-CN/docs/Web/HTML',
+          },  {
               text: 'LeetCode',
               link: 'https://leetcode-cn.com/',
           }, {
               text: 'Vue.js',
               link: 'https://cn.vuejs.org/v2/guide/',
-          }, {
-              text: 'BootCDN',
-              link: 'https://www.bootcdn.cn/',
-          }, {
-              text: 'Linux命令大全',
-              link: 'https://www.linuxcool.com/',
-          }, {
+          },  {
               text: '编程语言排行榜',
               link: 'https://www.tiobe.com/tiobe-index/',
           }]
